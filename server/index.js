b58a871a877db884cc0dea200147e0ada5105114
hr097 <harshilramani9777@gmail.com>
2023-01-22 11:05:33 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+//TODO: DEPENDENCIES
+
+const path = require('path');
+const express = require('express');
+const app = express(); 
+const logger = require("morgan");
+const cors = require("cors");
+
+//TODO: CONFIGURATION
+
+app.use(logger("dev"));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+
+
+const port = process.env.PORT || 3000;
+
+app.get("/test", (req, res) => {
+    return res.send("It's good to go!");
+  });
+  
+  app.get("/",(req,res)=>{
+      app.use(express.static(path.resolve(path.resolve(__dirname,".."), 'savaliya_technology')))
+      res.sendFile(path.resolve(path.resolve(__dirname,".."),'savaliya_technology','index.html'))
+  })
+  
+  app.listen(port, () => console.log(`Server Running on port ${port}`));
+  module.exports = app;
